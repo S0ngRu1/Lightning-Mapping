f2c9d942962a4ca1aa454ad14a207c92b5066852
S0ngRu1 <1922909737@qq.com>
2025-02-20 22:38:01 +0800

    1024 match

--- if pulse_matching/get_offset_yld_chj_siddle.m
+++ if pulse_matching/get_offset_yld_chj_siddle.m
@@ -9,9 +9,11 @@ all_R_gccs = [];
 all_t_gccs = [];
 filtered_yld_signal = filter_bp(yld_signal, 20e6 ,80e6 ,5);
 processed_yld_signal = real(windowsignal(detrend(filtered_yld_signal)));
-subsignal_length = yld_signal_length/8;
+subsignal_length = yld_signal_length/4;
 subsignal_start = 1:subsignal_length:chj_signal_length;
+h = waitbar(0, 'Processing...'); 
 for subi = 1:numel(subsignal_start)
+    waitbar(subi/numel(subsignal_start), h, sprintf('Processing %.2f%%', subi/numel(subsignal_start)*100));
     if subsignal_start(subi)+yld_signal_length-1 > chj_signal_length
         continue
     end
@@ -30,6 +32,6 @@ start_read_loc_chj_top10 = zeros(1, 10);
 for i = 1:10
     start_read_loc_chj_top10(i) = chj_signal_start_loc + subsignal_start(top10_r_gcc_idxs(i)) + floor(all_t_gccs(top10_r_gcc_idxs(i)));
 end
-
+close(h); 
 offset_result = start_read_loc_chj_top10 - yld_signal_start_loc;
 disp(offset_result)
